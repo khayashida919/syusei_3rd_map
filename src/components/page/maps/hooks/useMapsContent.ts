b77f1161a685fa816701850f58cd903a1c99b0b7
林田和樹 <>
2024-06-12 22:57:18 +0900

    データ途中まで追加

--- src/components/page/maps/hooks/useMapsContent.ts
+++ src/components/page/maps/hooks/useMapsContent.ts
@@ -83,8 +83,9 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0004 兵庫県神戸市中央区中山手通１丁目１７−１４ 東門タワービル 9F',
+      googleLink: 'https://maps.app.goo.gl/oFQYDsDo5PhvJPAz7',
       lat: 34.6961489,
       lng: 135.1911242,
       memo: '',
@@ -92,8 +93,8 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address: '〒650-0001 兵庫県神戸市中央区加納町４丁目７−２４ かのう屋',
+      googleLink: 'https://maps.app.goo.gl/hjRAaRRuJyBXDMRy8',
       lat: 34.6958281,
       lng: 135.1928604,
       memo: '',
@@ -101,7 +102,8 @@ export const useMapsContent = () => {
       state: 'full',
     },
     {
-      address: '',
+      address:
+        '〒650-0004 兵庫県神戸市中央区中山手通１丁目１５−２ 東門エースタウンビル',
       googleLink: '',
       lat: 34.6957834,
       lng: 135.1914795,
@@ -110,7 +112,7 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
+      address: '〒650-0004 兵庫県神戸市中央区中山手通２丁目１０−２２',
       googleLink: '',
       lat: 34.6964898,
       lng: 135.190564,
@@ -119,7 +121,8 @@ export const useMapsContent = () => {
       state: 'low',
     },
     {
-      address: '',
+      address:
+        '〒650-0004 兵庫県神戸市中央区中山手通１丁目８−１１ 北野坂Ｆビル 9f',
       googleLink: '',
       lat: 34.6954372,
       lng: 135.1924749,
@@ -128,7 +131,7 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
+      address: '〒650-0011 兵庫県神戸市中央区下山手通２丁目１０',
       googleLink: '',
       lat: 34.693529,
       lng: 135.1909125,
@@ -137,7 +140,7 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
+      address: '〒650-0001 兵庫県神戸市中央区加納町４丁目７−８',
       googleLink: '',
       lat: 34.6960416,
       lng: 135.1925014,
@@ -146,7 +149,7 @@ export const useMapsContent = () => {
       state: 'low',
     },
     {
-      address: '',
+      address: '〒650-0001 兵庫県神戸市中央区加納町４丁目９−９',
       googleLink: '',
       lat: 34.6960523,
       lng: 135.1931371,
@@ -155,7 +158,7 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
+      address: '〒650-0004 兵庫県神戸市中央区中山手通１丁目７−１８−１',
       googleLink: '',
       lat: 34.6951544,
       lng: 135.1923604,
@@ -164,7 +167,8 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
+      address:
+        '〒650-0004 兵庫県神戸市中央区中山手通１丁目１７−１４ 東門タワービル 9f',
       googleLink: '',
       lat: 34.6961489,
       lng: 135.1911242,
@@ -173,7 +177,7 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
+      address: '〒650-0004 兵庫県神戸市中央区中山手通１丁目７−７ 格子屋ビル',
       googleLink: '',
       lat: 34.6950326,
       lng: 135.1922477,
@@ -182,7 +186,7 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
+      address: '〒650-0001 兵庫県神戸市中央区加納町４丁目７−２６ 藤嶋ビル b1',
       googleLink: '',
       lat: 34.6961032,
       lng: 135.1929554,
@@ -191,8 +195,9 @@ export const useMapsContent = () => {
       state: 'full',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0001 兵庫県神戸市中央区加納町４丁目９−１２ クリスタルコートビル 3f',
+      googleLink: 'https://maps.app.goo.gl/LSKr63YWyxQGGjQt7',
       lat: 34.6963237,
       lng: 135.1935014,
       memo: '',
@@ -200,8 +205,9 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0001 兵庫県神戸市中央区加納町４丁目８−１４ 第１１シャルマンビル',
+      googleLink: 'https://maps.app.goo.gl/7YFJvAYq9DeqHP1V8',
       lat: 34.6965353,
       lng: 135.19265,
       memo: '',
@@ -209,8 +215,9 @@ export const useMapsContent = () => {
       state: 'full',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0011 兵庫県神戸市中央区下山手通２丁目１７−１０ ライオンビル三宮館 002',
+      googleLink: 'https://maps.app.goo.gl/31AMw8nt5NSFEvE88',
       lat: 34.6942743,
       lng: 135.1899407,
       memo: '',
@@ -218,8 +225,9 @@ export const useMapsContent = () => {
       state: 'full',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0001 兵庫県神戸市中央区加納町４丁目８−１２ 北野坂クイーンズビル 3Ｆ',
+      googleLink: 'https://maps.app.goo.gl/VDobskh5LobgdR816',
       lat: 34.6966416,
       lng: 135.1923574,
       memo: '',
@@ -227,8 +235,8 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address: '〒650-0011 兵庫県神戸市中央区下山手通３丁目１０−１４',
+      googleLink: 'https://maps.app.goo.gl/E43ykBsFEtP8tkYQ6',
       lat: 34.6930769,
       lng: 135.1883947,
       memo: '',
@@ -236,8 +244,9 @@ export const useMapsContent = () => {
       state: 'full',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0011 兵庫県神戸市中央区下山手通１丁目５−４ コウベコンベックススクエア',
+      googleLink: 'https://maps.app.goo.gl/2XBL3xKSE4LBWqWx7',
       lat: 34.6952331,
       lng: 135.1910405,
       memo: '',
@@ -245,8 +254,9 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0011 兵庫県神戸市中央区下山手通１丁目５−４ コウベコンベックススクエア 407',
+      googleLink: 'https://maps.app.goo.gl/2XBL3xKSE4LBWqWx7',
       lat: 34.6952325,
       lng: 135.1910953,
       memo: '',
@@ -254,8 +264,9 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0001 兵庫県神戸市中央区加納町４丁目７−１１ パレ・ド・北野坂 9階',
+      googleLink: 'https://maps.app.goo.gl/UVE8bdphFKHHSyid6',
       lat: 34.696094,
       lng: 135.1926317,
       memo: '',
@@ -263,8 +274,9 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0004 兵庫県神戸市中央区中山手通１丁目１７−６ ハイム北野フェニックス 1F H102',
+      googleLink: 'https://maps.app.goo.gl/1bTJUvzSFgUwwqgF9',
       lat: 34.6959665,
       lng: 135.1913149,
       memo: '',
@@ -272,8 +284,8 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address: '〒650-0004 兵庫県神戸市中央区中山手通１丁目４−１',
+      googleLink: 'https://maps.app.goo.gl/fDEN4g3krCWf7mYu6',
       lat: 34.693819,
       lng: 135.191847,
       memo: '',
@@ -281,8 +293,9 @@ export const useMapsContent = () => {
       state: 'empty',
     },
     {
-      address: '',
-      googleLink: '',
+      address:
+        '〒650-0012 兵庫県神戸市中央区北長狭通１丁目２１−９ Ｃｏｌｌｅｃｔｉｏｎ Ｋ ３階',
+      googleLink: 'https://maps.app.goo.gl/4VCHnKn6sxexkkGp9',
       lat: 34.694488,
       lng: 135.1923731,
       memo: '',
